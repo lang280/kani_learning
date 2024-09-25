e6d3fd391e2e90d967522e92fa60c8d02f1d48f8
lang280 <aaronlang280@outlook.com>
2024-09-25 03:30:44 -0400

    first-steps-v1

--- First_steps/target/debug/deps/First_steps-2bef9c120a461018.d
+++
@@ -1,5 +0,0 @@
-/Users/admin0/Documents/GitHub/kani_learning/First_steps/target/debug/deps/First_steps-2bef9c120a461018: src/main.rs
-
-/Users/admin0/Documents/GitHub/kani_learning/First_steps/target/debug/deps/First_steps-2bef9c120a461018.d: src/main.rs
-
-src/main.rs:
